a59652e6f2e3c8622d4ba568a71a7e88e600f34d
Benedikt <benediktb11@tuta.io>
2021-02-14 18:49:41 +0100

    Added getip()-> String, which returns the current IP

--- linkage_leaks/src/lib.rs
+++ linkage_leaks/src/lib.rs
@@ -1,3 +1,22 @@
+use curl::easy::Easy;
+
+fn getip()->String{
+    let mut easy = Easy::new();
+    easy.url("https://am.i.mullvad.net/ip").unwrap();
+    let mut transfer = easy.transfer();
+    transfer.write_function(|data|{
+        ip = data;
+        Ok(data.len())
+    }).unwrap();
+    transfer.perform().unwrap();
+    ip
+}
+
+
+
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
