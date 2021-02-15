e77f0644309fe81ab5a390bab7df1303f5ba2968
ravi0li <moritz@howtoflex.club>
2021-02-15 11:47:57 +0100

    add executor_execute_for macro

--- linkage_firewall/src/utils.rs
+++ linkage_firewall/src/utils.rs
@@ -14,8 +14,25 @@ macro_rules! to_string_vec {
     };
 }
 
+/// Executes the supplied expression for the supplied identifiers. Required a .execute method that
+/// can accept the expression and returns a Result.
+#[macro_export]
+macro_rules! executor_execute_for {
+    ( $x:expr, $( $e:ident ),+ ) => {
+        {
+            $(
+                $e.execute($x)?;
+            )+
+        }
+    };
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::executor::{Executor, MockExecutor};
+    use mockall::predicate::*;
+    use crate::error::FirewallError;
+
     #[test]
     fn test_to_string_vec() {
         let v1: Vec<String> = vec![];
@@ -25,6 +42,10 @@ mod tests {
             v2,
         );
         assert_eq!(
+            vec![String::from("Hello")],
+            to_string_vec!("Hello"),
+        );
+        assert_eq!(
             vec![String::from("Hello"), String::from("World")],
             to_string_vec!("Hello", "World"),
         );
@@ -32,5 +53,35 @@ mod tests {
             vec![String::from("Hello"), String::from("World"), String::from("!!!")],
             to_string_vec!("Hello", "World", "!!!"),
         );
+    }
+
+    #[test]
+    fn test_executor_execute_for() -> Result<(), FirewallError> {
+        let mut e1_mock = MockExecutor::new();
+        e1_mock.expect_execute()
+            .with(eq(to_string_vec!("hello", "world", "420")))
+            .returning(|_| Ok(()));
+        e1_mock.expect_execute()
+            .with(eq(to_string_vec!("test", "abc")))
+            .returning(|_| Ok(()));
+
+        let mut e2_mock = MockExecutor::new();
+        e2_mock.expect_execute()
+            .with(eq(to_string_vec!("test", "abc")))
+            .returning(|_| Ok(()));
+        e2_mock.expect_execute()
+            .with(eq(to_string_vec!("cat")))
+            .returning(|_| Err(FirewallError::IptablesError(None)));
+
+        executor_execute_for!(to_string_vec!("hello", "world", "420"), e1_mock);
+        executor_execute_for!(to_string_vec!("test", "abc"), e1_mock, e2_mock);
+
+        // It should return the error if one occurred
+        assert!(|| -> Result<(), FirewallError> {
+            executor_execute_for!(to_string_vec!("cat"), e2_mock);
+            Ok(())
+        }().is_err());
+
+        Ok(())
     }
 }
