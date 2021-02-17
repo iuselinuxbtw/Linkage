6f071dcfd5f88f6025ff5e8c55f874f369648898
Benedikt <benediktb11@tuta.io>
2021-02-17 17:37:46 +0100

    Added request.rs

---
+++ linkage_leaks/src/request.rs
@@ -0,0 +1,27 @@
+#[cfg(test)]
+use mockall::automock;
+use std::io::Read;
+use crate::error::HttpError;
+
+pub enum RequestError {
+
+}
+
+pub(crate) type RequestResult<T> = Result<T, RequestError>;
+
+#[cfg_attr(test, automock)]
+pub trait Requester {
+    /// Returns the body of a given Internet address
+    fn get(&self, address: &str) -> RequestResult<String>;
+}
+
+pub struct InternetRequester {}
+
+impl Requester for InternetRequester {
+    fn get(&self, address: &str) -> RequestResult<String> {
+        let mut response = reqwest::blocking::get(address).map_err(|_|HttpError::ResponseError).unwrap();
+        let mut body = String::new();
+        response.read_to_string(&mut body).unwrap();
+        Ok(body)
+    }
+}
