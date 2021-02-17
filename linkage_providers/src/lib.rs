4fff72134316fbe664f80059785f2b73ce2f0b94
ravi0li <moritz@howtoflex.club>
2021-02-17 17:34:05 +0100

    add linkage_providers lib

---
+++ linkage_providers/src/lib.rs
@@ -0,0 +1,4 @@
+//! This crate contains everything necessary to configure VPN providers. VPN providers provide
+//! specific configurations that the Linkage application can use to connect to VPN servers.
+//! Additionally, this library contains support for authentication for these providers. Every
+//! has to be implemented on its own using the provided toolkit.
