e370b387ae606e782ba65789691d14c2f58441cf
ravi0li <moritz@howtoflex.club>
2021-02-18 22:14:15 +0100

    accept any amount for dns leak test amount and adjust it according to constant

--- linkage_leaks/src/lib.rs
+++ linkage_leaks/src/lib.rs
@@ -59,25 +59,60 @@ pub fn get_ip_information() -> LeakResult<IpInformation> {
     Ok(infos)
 }
 
-/// Returns a list of all detected DNS servers. The supplied argument amount determines how often
-/// the test will be run.
-pub fn dns_test(amount: u32) -> LeakResult<Vec<IpAddr>> {
-    // At first, we have to calculate the amount of threads that should be used. Currently, only
-    // multiple of LEAK_DETECT_REQUESTS_PER_THREAD can be used so this will even work.
-    // TODO: Change this behaviour
-    let amount_of_threads = amount / LEAK_DETECT_REQUESTS_PER_THREAD;
+/// Options related to the execution of the dns leak test.
+#[derive(Debug, PartialEq)]
+struct DnsTestConfig {
+    total: u32,
+    requests_per_thread: u32,
+    amount_of_threads: u32,
+}
+
+/// Returns the configuration that should be used to execute the dns leak test. This mainly consists
+/// of data related to the amount of threads and the requests that should be executed in each
+/// thread.
+fn get_dns_leak_test_config(mut amount_of_requests: u32) -> DnsTestConfig {
+    // Not mutable because it should be equal to the constant
+    let requests_per_thread: u32 = LEAK_DETECT_REQUESTS_PER_THREAD;
+
+    // If the amount of requests that should be executed is a multiple of requests_per_thread we can
+    // safely use the provided value
+    if amount_of_requests % requests_per_thread != 0 {
+        // If the amount is not a multiple of the constant, we have to get the next multiple of the
+        // amount
+        amount_of_requests = {
+            let mut t: u32 = 0;
+            while t < amount_of_requests {
+                t += requests_per_thread;
+            }
+            t
+        };
+    }
+    let amount_of_threads: u32 = amount_of_requests / requests_per_thread;
+
+    return DnsTestConfig {
+        total: amount_of_requests,
+        requests_per_thread,
+        amount_of_threads,
+    }
+}
+
+/// Returns a list of all detected DNS servers. The supplied argument amount_of_requests determines
+/// how often the test will be run.
+pub fn dns_test(amount_of_requests: u32) -> LeakResult<Vec<IpAddr>> {
+    let config = get_dns_leak_test_config(amount_of_requests);
 
     // Make a channel to receive the ip addresses
     let (tx, rx): (Sender<_>, Receiver<_>) = mpsc::channel::<String>();
     let mut children = Vec::new();
 
-    for _ in 0..amount_of_threads {
+    for _ in 0..config.amount_of_threads {
         let thread_tx = tx.clone();
+        let per_thread = config.requests_per_thread.clone();
 
         // Spawn the threads and save their handles into a Vec
         let child = thread::spawn(move || {
             // Each thread should do a specific amount of requests per thread
-            for _ in 0..LEAK_DETECT_REQUESTS_PER_THREAD {
+            for _ in 0..per_thread {
                 // TODO: Improve the error handling for this calls
                 let dns = get_dns().expect("cannot get dns server for dns leak detection");
                 // We use .except here since this will probably not happen because we calculate this
@@ -90,7 +125,7 @@ pub fn dns_test(amount: u32) -> LeakResult<Vec<IpAddr>> {
 
     // Save the collected ip addresses into a vec
     let mut ips: Vec<IpAddr> = Vec::new();
-    for _ in 0..amount {
+    for _ in 0..config.total {
         ips.push(rx.recv()?.parse()?);
     }
 
@@ -142,5 +177,48 @@ mod tests {
             assert_eq!(40, s.len());
             assert!(s.chars().all(|x| x.is_ascii_digit() || x.is_ascii_lowercase()));
         }
+    }
+
+    #[test]
+    fn test_get_dns_leak_test_config() {
+        assert_eq!(DnsTestConfig {
+            total: 25,
+            requests_per_thread: 5,
+            amount_of_threads: 5,
+        }, get_dns_leak_test_config(25));
+
+        assert_eq!(DnsTestConfig {
+            total: 100,
+            requests_per_thread: 5,
+            amount_of_threads: 20,
+        }, get_dns_leak_test_config(100));
+
+        assert_eq!(DnsTestConfig {
+            total: 75,
+            requests_per_thread: 5,
+            amount_of_threads: 15,
+        }, get_dns_leak_test_config(74));
+        assert_eq!(DnsTestConfig {
+            total: 80,
+            requests_per_thread: 5,
+            amount_of_threads: 16,
+        }, get_dns_leak_test_config(76));
+        assert_eq!(DnsTestConfig {
+            total: 80,
+            requests_per_thread: 5,
+            amount_of_threads: 16,
+        }, get_dns_leak_test_config(79));
+
+        assert_eq!(DnsTestConfig {
+            total: 0,
+            requests_per_thread: 5,
+            amount_of_threads: 0,
+        }, get_dns_leak_test_config(0));
+
+        assert_eq!(DnsTestConfig {
+            total: 5,
+            requests_per_thread: 5,
+            amount_of_threads: 1,
+        }, get_dns_leak_test_config(1));
     }
 }
